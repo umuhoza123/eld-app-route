e0130ea3058fbb18a659d6faee1156e4505d8a42
blandineU <brandineumuhoza@gmail.com>
2025-11-13 23:43:02 +0200

    updateed

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -131,6 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://eld-app-route.onrender.com/',
+     'https://*.vercel.app',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
